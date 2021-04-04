538f02095f38e9aa5e5a977134aa23f90f0eb2e0
kimseonjae <rlatjswowkd@gmail.com>
2021-04-04 16:49:02 +0900

    first commit

---
+++ helloworld/views.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def hello1(request):
+    return HttpResponse('<h1>Hello World1</h1>', content_type='text/html; charset=utf-8')
+
+
+def hello2(request):
+    return render(request, 'helloworld/hello2.html')
+
+
+def tags(request):
+    return render(request, 'helloworld/tags.html')
